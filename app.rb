c03ec5f55e41f77128d390a3d91b015f31925dd7
joemaidman <joemaidman@gmail.com>
2017-03-08 17:50:43 +0000

    Adding heroku database connection

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require './lib/link'
 require './lib/server'
 
 ENV['RACK_ENV'] ||= 'development'
-ENV['DATABASE_URL'] = "postgres://fqfdmrjejzykxg:841cf93393aeb1b90d90f6f53109928f2cce3dfbfb0c703a1645569d83604c7c@ec2-54-235-92-236.compute-1.amazonaws.com:5432/d5e12dm1afqi8i"
+#ENV['DATABASE_URL'] = "postgres://fqfdmrjejzykxg:841cf93393aeb1b90d90f6f53109928f2cce3dfbfb0c703a1645569d83604c7c@ec2-54-235-92-236.compute-1.amazonaws.com:5432/d5e12dm1afqi8i"
 
 class Bookmark < Sinatra::Base
 
