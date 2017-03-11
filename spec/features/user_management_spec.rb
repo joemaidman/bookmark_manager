2a59781366a41b97799669d50ee7c856ddc76352
joemaidman <joemaidman@gmail.com>
2017-03-11 17:32:39 +0000

    Adding user sign-in

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -16,14 +16,39 @@ feature 'User sign up' do
                       password_confirm: password_wrong) 
                     }.not_to change(User, :count)
       expect(current_path).to eq('/users')
-      expect(page).to have_content "Password and confirmation password did not match!"
+      expect(page).to have_content "Password does not match the confirmation"
   end
 
   scenario 'cannot sign-up with an empty email address' do
     expect{ sign_up(email: nil, password: password )}.not_to change(User, :count)
+    expect(page).to have_content('Email must not be blank')
   end
 
    scenario 'cannot sign-up with an invalid email addres' do
     expect{ sign_up(email: email_wrong, password: password )}.not_to change(User, :count)
+    expect(page).to have_content('Email has an invalid format')
+  end
+  
+  scenario 'cannot create a new account with the same email address' do
+    sign_up(email:email, password: password)
+    expect{sign_up(email:email, password: password)}.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
+
+feature 'User sign in' do
+ let!(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
+  end
+
+  scenario 'has correct email and password to login' do
+    sign_in(email: user.email,   password: user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
+
+  end
+
+
+  end
+  
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -14,3 +14,10 @@ def sign_up(email:, password:, password_confirm: password)
   fill_in :password_confirmation, with: password_confirm
   click_button 'Sign Up'
 end
+
+def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign In'
+  end
